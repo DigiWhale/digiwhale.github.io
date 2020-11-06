268786673b2e6f214eae79d36c9a07b185576079
digiwhale <digiwhale.humphrey@gmail.com>
2020-11-06 12:39:17 -0500

    added svg animation for desk

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // const x = require("snapsvg")
 window.onload = play();
-document.getElementById('logo').addEventListener('wheel', () => { play() })
+document.getElementById('svg').addEventListener('click', () => { play() })
+
+
 
 
 function play() {
@@ -21,7 +23,6 @@ setTimeout( function() {
 var logoTitle = 'Andrew Humphrey';
 var logoRandom = '';
 var logoTitleContainer = l.text(0, '50%', '');
-console.log(l.text)
 var possible = "-+*/|}{[]~\\\":;?/.><=+-_)(*&^%$#@!)}";
 logoTitleContainer.attr({
 fontSize: 280,
@@ -47,3 +48,45 @@ logoRandom = '';
 }, 500 );
 
 }
+
+var tl = new TimelineMax();
+var bgd = $('#background rect');
+var table = $('#table_legs, #table');
+var lampLeg = $('#lamp > .lamp-leg');
+var lampbt = $('#lamp-bottom');
+var lampLight = $('#lamp > .light');
+var lampLine = $('#lamp-line');
+var lampLineB = $('#lamp-line-b');
+var lampLineT = $('#lamp-line-t');
+var lampCircle = $('#lamp-circle');
+var lampHead = $('#lamp-head');
+var lampHeader = $('#lamp-header');
+var lampBody = $('#lamp-body');
+var computer = $('#computer > *');
+var keyboard = $('#keyboard > *');
+var asset = $('#computer_mouse > * , #coffee_mug > *');
+
+tl.from(bgd, 0.02, {opacity:0, scale:0, transformOrigin: 'center center'})
+.staggerFrom(table, 0.02, {y:"-=200", opacity: 0, ease: Elastic.easeOut}, 0.01)
+.from(lampLeg, 0.02, {opacity:0, x: "-200", ease: Elastic.easeOut})
+.from(lampbt, 0.02, {opacity:0, scale:0, transformOrigin: 'center center'})
+.from(lampLineB, 0.3,{opacity:0, transformOrigin: '100% 100%', rotation: '-180deg'})
+.from(lampCircle,0.1,{opacity:0, x: '-=100', y: '-=100'})
+  .from(lampLineT, 0.3,{opacity:0, transformOrigin: '0% 100%', rotation: '-180deg'})
+.from(lampHead, 0.02, {opacity:0, scale:0, ease: Elastic.easeOut})
+.from(lampHeader, 0.05, {transformOrigin: '60% 60%', rotation: '60deg'})
+.from(lampBody, 0.5, {transformOrigin: '70% 70%', rotation: '-25deg'})
+.staggerFrom(computer, 01, {opacity: 0, scale: 0, transformOrigin: 'center center', ease: Back .easeOut}, 0.02)
+.staggerFrom(keyboard, 0.05, {opacity: 0, y: '-=100', ease: Linear.easeInOut }, 0.05)
+.staggerFrom(asset, 0.05, {opacity: 0}, 0.05)
+.to(lampLight, 0.02, {opacity:0.8, ease: Elastic.easeOut, delay:0.1}, "a")
+  .to(lampLight, 0.01, {opacity:0}, "b")
+  .to(lampLight, 0.01, {opacity:0.2}, "c")
+.to(bgd, 0.02, {opacity: 0.1, delay:0.1}, "a-=0.05")
+.to(bgd, 0.01, {opacity: 1}, "b-=0.05")
+.to(bgd, 0.01, {opacity: 0.5}, "c-=0.05")
+// .to(bgd, 0.02, {opacity: 1, fill: '#FDD10D'})
+.fromTo(lampLine, 0.02, {opacity: 0},{opacity: 0.2, delay:0.1}, "a-=0.05")
+.to(lampLine, 0.01, {opacity: 1}, "b-=0.05")
+.to(lampLine, 0.01, {opacity: 0.5}, "c-=0.05");
+document.getElementById('console_text').textContent="Andrew_Humphrey@mac:~ $ Sudo Hire-Me.py"
