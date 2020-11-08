b60864e54a043902bea11ffcb1f5b5c139ee34e0
digiwhale <digiwhale.humphrey@gmail.com>
2020-11-08 15:33:58 -0500

    added sticky nav bar

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ build_svg()
 
 // Terminal javascript
 var term;
-$('.footer').terminal({
+$('#terminal').terminal({
   fuck: function() {
       this.echo("No thanks!");
   },
@@ -66,3 +66,24 @@ $('.footer').terminal({
 document.documentElement.style.setProperty('--animate-duration', '10s');
 document.documentElement.style.setProperty('--animate-duration', '.5s');
 
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = 30;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+$(document).ready(function(){
+  $(this).scrollTop(0);
+});
