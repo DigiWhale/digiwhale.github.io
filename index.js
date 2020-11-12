d48c6d83c8c547adf4380b7626146cee94a92870
digiwhale <digiwhale.humphrey@gmail.com>
2020-11-12 02:06:24 -0500

    added matrix feature and help commands

--- index.js
+++ index.js
@@ -58,14 +58,17 @@ build_svg()
 // Terminal javascript
 var term;
 $('#terminal').terminal({
-  fuck: function() {
-      this.echo("[[g;green;black]some text]");
+  hack: function() {
+      runMatrix();
+  },
+  help: function() {
+    this.echo("[[;green;black]- hack\n- cat\n- clear]");
   },
   cat: function() {
     this.echo($('<img src="https://placekitten.com/408/287" width=50px>'));
 }
 }, {
-  greetings: "[[;blue;black]Welcome to my world.]"
+  greetings: "[[;blue;black]Welcome to my world. Type help for list of commands.]"
 }
 );
 
@@ -147,4 +150,48 @@ function show_videos() {
     youtubeVideos.style.visibility = "hidden";
     youtubeVideos.style.height = "0px";
   }
+}
+
+// Get the canvas node and the drawing context
+const canvas = document.getElementById('canv');
+const ctx = canvas.getContext('2d');
+
+// set the width and height of the canvas
+const w = canvas.width = document.body.offsetWidth;
+const h = canvas.height = document.body.offsetHeight;
+
+// draw a black rectangle of width and height same as that of the canvas
+ctx.fillStyle = '#000';
+ctx.fillRect(0, 0, w, h);
+const cols = Math.floor(w / 20) + 1;
+const ypos = Array(cols).fill(0);
+
+function matrix () {
+  // Draw a semitransparent black rectangle on top of previous drawing
+  ctx.fillStyle = '#0001';
+  ctx.fillRect(0, 0, w, h);
+
+  // Set color to green and font to 15pt monospace in the drawing context
+  ctx.fillStyle = '#0f0';
+  ctx.font = '15pt monospace';
+
+  // for each column put a random character at the end
+  ypos.forEach((y, ind) => {
+    // generate a random character
+    const text = String.fromCharCode(Math.random() * 128);
+
+    // x coordinate of the column, y coordinate is already given
+    const x = ind * 20;
+    // render the character at (x, y)
+    ctx.fillText(text, x, y);
+
+    // randomly reset the end of the column if it's at least 100px high
+    if (y > 100 + Math.random() * 10000) ypos[ind] = 0;
+    // otherwise just move the y coordinate for the column 20px down,
+    else ypos[ind] = y + 20;
+  });
+}
+function runMatrix(){
+  // render the animation at 20 FPS.
+  setInterval(matrix, 50);
 }
